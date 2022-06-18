527cace4480cbe3afbf855ddac2a80f6f2fcaa56
Sarthak <sarthaksontakke6124.com>
2022-06-18 15:27:07 +0530

    Initial Commit

---
+++ ex/ex_1.java
@@ -0,0 +1,14 @@
+class ex_1{
+public static void main(String args[] ) {
+
+System.out.print("*\n**\n***\n****\n*****");
+
+
+}
+
+
+
+
+
+
+}
---
+++ hello.java
@@ -0,0 +1,16 @@
+class hello{
+
+public static void main(String args[]){
+System.out.println("hello world");
+System.out.print("how are you \n");
+System.out.println("are we good  ");
+
+}
+
+
+
+
+
+
+
+}
